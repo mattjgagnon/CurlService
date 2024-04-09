cdc898f7e8773a53031a55c211b3860b3e301785
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-04-09 19:01:12 -0400

    Refactor exception to extract const

--- src/CurlRetryException.php
+++ src/CurlRetryException.php
@@ -6,10 +6,12 @@ use RuntimeException;
 
 final class CurlRetryException extends RuntimeException
 {
+    private const MSG_FORMAT = "Failed to fetch data from %s after %d attempts. Response code: %d. Response body: %s";
+
     public function __construct(string $url, int $statusCode, string $responseBody, int $maxAttempts)
     {
         $message = sprintf(
-            "Failed to fetch data from %s after %d attempts. Response code: %d. Response body: %s",
+            self::MSG_FORMAT,
             $url,
             $maxAttempts,
             $statusCode,
